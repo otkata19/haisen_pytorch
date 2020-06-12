89c1aa99c686b24a17feaa921d1ee09a3079abae
tanakakeitaro <1968kocm1akouk@gmail.com>
2020-06-13 01:50:25 +0900

    mecabからjanomeへ移行

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn haisen_pytorch.wsgi --log-file -
--- mecab_test.py
+++ mecab_test.py
@@ -1,20 +1,12 @@
 import re
 import io,sys
-import MeCab
-import subprocess
-
-cmd='echo `mecab-config --dicdir`"/mecab-ipadic-neologd"'
-path = (subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True).communicate()[0]).decode('utf-8')
-m=MeCab.Tagger("-d {0}".format(path))
+from janome.tokenizer import Tokenizer
 
 def user_input(text):
     # 記号の削除
     clean_i = re.sub(re.compile('[!"#$%&\'\\\\()*+,-./:;<=>?@[\\]^_`{|}~「」〔〕“”〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％]'), '', text)
     sentence = clean_i.replace("　", "").replace(" ", "") # スペースの消去
-    node = m.parseToNode(sentence).next #解析してノードに変換
-    word_list = []
-    while node.next: #nodeがfalseになるまで
-        word_list.append(node.surface)
-        node = node.next #次の要素に移動（末尾の場合はfalseになる）
-
+    # janomeで形態素解析
+    t = Tokenizer()
+    word_list = [token.surface for token in t.tokenize(sentence)]
     return word_list
